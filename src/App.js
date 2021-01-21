29a7df6f8772307427a2e3c0aa255a9bfd01b782
Gannon Carroll <gmc384@humboldt.edu>
2021-01-21 14:42:16 -0800

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Nav, Navbar, FormControl, Form, Button, NavDropdown } from 'react-bootstrap';
+
 
 const list = [
   {
@@ -24,8 +27,8 @@ const welcome = {
   title: 'React',
 }
 
-const numbers = [1,4,9,16];
-const newNumbers = numbers.map(function(number){
+const numbers = [1, 4, 9, 16];
+const newNumbers = numbers.map(function (number) {
   return number * 2;
 });
 
@@ -34,38 +37,67 @@ function App() {
 
   const title = 'React';
   let some_var = 414;
-  
+  console.log(some_var);
   return (
-   <div className = "App">
-     <h1>
-       {welcome.greeting}{welcome.title}
-     </h1>
+    <Container>
 
-     <label htmlFor="search">Search: </label>
-     <input id = "search" type = "text"/>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+      </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+
+      <Row>
+        <Col>
+          <h1>
+            {welcome.greeting}{welcome.title}
+          </h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <label htmlFor="search">Search: </label>
+          <input id="search" type="text" />
+        </Col>
+      </Row>
+
+
+
       <hr />
-    {list.map( (item) => {
-      return <div key = {item.title}>
-        <span>
-          <a href = {item.url}>{item.title}</a>
-        </span>
-        <span> {item.author}</span>
-        <span> {item.num_comments}</span>
-        <span> {item.points}</span>
-      </div>
-    })}
- 
-   </div>
-   
+     
+      
+     
+
+    </Container>
+
   );
 }
 
 function List() {
-  return list.map(function(item){
+  return list.map(function (item) {
     return (
-      <div key = {item.objectID}>
+      <div key={item.objectID}>
         <span>
-          <a href = {item.url}>{item.title}</a>
+          <a href={item.url}>{item.title}</a>
         </span>
         <span> {item.author}</span>
         <span> {item.num_comments}</span>
